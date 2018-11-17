45147e523a708ab9e91b787e7d50d6410a353817
ajbinkley <42150496+ajbinkley@users.noreply.github.com>
2018-11-17 11:32:24 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,1 @@
+nyt_api_key = "98b80a887d1d438c8c6e409affc7cf17"
---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Create and use customer_db
+CREATE DATABASE movie_data_db;
+USE movie_data_db;
+
+-- Create tables for raw data to be loaded into
+CREATE TABLE nyt (
+id INT PRIMARY KEY,
+title_nyt TEXT,
+byline_nyt TEXT,
+open_date_nyt TEXT
+);
+
+CREATE TABLE omdb (
+id INT PRIMARY KEY,
+title_omdb TEXT,
+omdb_year TEXT,
+omdb_id TEXT
+);
+
+
+-- Join tables in MySQL
+select * from nyt
+join omdb on omdb.title_omdb = nyt.title_nyt;
